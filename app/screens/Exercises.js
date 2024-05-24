692f0688a68143fcd83a5f4cb70e1b9e45227d83
Laurie Clifton <laurieclifton@Lauries-MacBook-Pro.local>
2024-05-24 11:20:34 +0100

    exercise component

--- app/screens/Exercises.js
+++ app/screens/Exercises.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, FlatList, TouchableOpacity} from "react-native";
+import { UserContext } from "../contexts/UserContext"
+import { Card } from "react-native-elements"
 
 const Exercises = () => {
+
+  const [exercises, setExercises] = useState([])
+
+  useEffect(() => {
+    
+  })
+
   return (
     <View style={styles.container}>
       <Image
         source={require("../../assets/placeholder_logo.jpeg")}
         style={styles.logo}
       />
-      <Text>Notifications Screen</Text>
+      <Text>My Exercises</Text>
+      <FlatList>
+
+      </FlatList>
     </View>
   );
 };
